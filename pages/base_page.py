0f71bd45f5753f5a5bf03be1745e45c746b87ea3
Alexandr <volokitin200@bk.ru>
2026-02-06 00:26:05 +0300

    added exception handling for element search

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 class BasePage():
     
     def __init__(self, browser, url):
@@ -6,3 +10,10 @@ class BasePage():
     
     def open(self):
         self.browser.get(self.url)
+    
+    def is_element_present(self, how, what):
+        try:
+            element = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True

138a4a3657fd5a98ca9258978a5076e330ea8bbe
Alexandr <volokitin200@bk.ru>
2026-02-06 01:37:41 +0300

    test: added product page test (adding book to basket)

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_CARD_BOOK_TITLE = (By.CSS_SELECTOR, '.product_main > h1')
+    POPUP_ADDED_TO_BASKET_BOOK_TITLE = (By.XPATH, "//*[@id='messages']//strong[1]")
+    HEADER_BASKET_TOTAL = (By.XPATH, "//header[contains(@class, 'header')]//div[contains(@class, 'basket')]")
+    POPUP_BASKET_TOTAL = (By.CSS_SELECTOR, '#messages > div:nth-child(3) .alertinner')
---
+++ pages/product_page.py
@@ -0,0 +1,46 @@
+
+import re
+import pytest
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+from utils.regex_templates import COST_REGEX
+
+class ProductPage(BasePage):
+    
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
+    
+    
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+    
+    
+    def should_be_added_to_basket(self) -> None:
+        if not self.is_element_present(*ProductPageLocators.PRODUCT_CARD_BOOK_TITLE):
+            pytest.fail("Book title in product card is not presented")
+        
+        if not self.is_element_present(*ProductPageLocators.POPUP_ADDED_TO_BASKET_BOOK_TITLE):
+            pytest.fail("Book title in 'added to basket' popup is not presented")
+        
+        product_card_book_title = self.browser.find_element(*ProductPageLocators.PRODUCT_CARD_BOOK_TITLE).text
+        alert_added_to_basket_book_title = self.browser.find_element(*ProductPageLocators.POPUP_ADDED_TO_BASKET_BOOK_TITLE).text
+        assert product_card_book_title == alert_added_to_basket_book_title, f"according to popup, added {alert_added_to_basket_book_title}, expected {product_card_book_title}"
+    
+    
+    def should_be_updated_price(self) -> None:
+
+        if not self.is_element_present(*ProductPageLocators.HEADER_BASKET_TOTAL):
+            pytest.fail("Basket total price is not presented in header")
+        
+        if not self.is_element_present(*ProductPageLocators.POPUP_BASKET_TOTAL):
+            pytest.fail("Basket total price is not presented in popup")
+
+        header_basket_total_text = self.browser.find_element(*ProductPageLocators.HEADER_BASKET_TOTAL).text
+        header_basket_total_price = re.findall(COST_REGEX, header_basket_total_text)[0]
+
+        popup_basket_total_text = self.browser.find_element(*ProductPageLocators.POPUP_BASKET_TOTAL).text
+        popup_basket_total_price = re.findall(COST_REGEX, popup_basket_total_text)[0]
+
+        assert header_basket_total_price == popup_basket_total_price, f"different total basket prices in header and popup (in header: {header_basket_total_price}, in popup: {popup_basket_total_price})"
+    
